1f2c73e9f9923a020f803f6588651f55d719d8c6
강지원 <kangji149@naver.com>
2023-06-19 00:05:44 +0900

    entity mapping

--- projects/jpa_basic/src/main/java/hellojpa/Member.java
+++ projects/jpa_basic/src/main/java/hellojpa/Member.java
@@ -1,15 +1,29 @@
 package hellojpa;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
+@Getter
+@Setter
 public class Member {
 
     @Id
     private Long id;
 
     private String name;
+
+    public Member(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Member() {
+
+    }
 
     public Long getId() {
         return id;
