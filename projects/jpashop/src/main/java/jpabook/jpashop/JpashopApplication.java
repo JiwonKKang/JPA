8d7ff6b40130925ad4fc7b0f6bce0e831903062c
강지원 <kangji149@naver.com>
2023-06-25 06:46:09 +0900

    jpq;l

--- projects/jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ projects/jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,13 +1,14 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Member;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.persistence.*;
+
 @SpringBootApplication
 public class JpashopApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(JpashopApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(JpashopApplication.class, args);
+    }
 }
--- projects/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ projects/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -5,7 +5,9 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -21,6 +23,12 @@ public class Member {
 
     @Embedded
     private Address address;
+
+    @CollectionTable(name = "favorite_food",
+            joinColumns = @JoinColumn(name = "member_id"))
+    @ElementCollection
+    @Column(name = "food_name")
+    private Set<String> favoriteFoods = new HashSet<>();
 
     @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)//읽기 전용, 연관관계의 주인은 order 테이블의 member 필드.
     private List<Order> orders = new ArrayList<>();
